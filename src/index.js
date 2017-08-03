d8fbcefff58d7029cd748bb76dcff55265e5e631
Greg Venech <greg.venech@gmail.com>
2017-08-03 14:53:40 -0400

    feat: switch to remark parser allowing plugins and output options to be passed
    Switch to remark (a more stable and pluggable parser), and allow
    `output` and `plugins` to be passed as loader options. `output` determines
    whether or not the `remark-react` or `remark-html` plugin is used for
    the final processing step while `plugins` allows passing any number
    of remark plugins.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+const Utils = require('loader-utils')
 const Parse = require('./parse.js')
-const Build = require('./build.js')
 
 /**
  * Primary loader function
@@ -8,9 +8,9 @@ const Build = require('./build.js')
  */
 module.exports = function(content) {
     const callback = this.async()
+    const options = Utils.getOptions(this)
 
-    Parse(content)
-        .then(Build)
-        .then(component => callback(null, component))
+    Parse(content, options)
+        .then(processed => callback(null, processed.content))
         .catch(callback)
 };
--- src/parse.js
+++ src/parse.js
@@ -1,57 +1,37 @@
-const FrontMatter = require('front-matter');
-const Remarkable = require('remarkable');
-const Parser = new Remarkable();
-
-/**
- * Parse a code block to have a source and a run code
- * 
- * @param   {string} code - Raw JSX code
- * @returns {string}      - Code block with souce and run code
- */
-function wrapJSX(code) {
-    return `
-        <div class="example">
-            ${code}
-        </div>
-    `
-}
-
-/**
- * Parse Markdown to HTML with code blocks
- * 
- * @param   {object} markdown - Markdown attributes and body
- * @returns {object}          - HTML and imports
- */
-function parseMarkdown(markdown) {
-    return new Promise((resolve, reject) => {
-        Parser.set({
-            xhtmlOut: true
-        })
-
-        Parser.renderer.rules.fence_custom.render = (tokens, idx, options) => {
-            return wrapJSX(
-                tokens[idx].content
-            )
-        }
-
-        try {
-            let html = Parser.render(markdown.body)
-            resolve({ html, attributes: markdown.attributes })
-
-        } catch (error) {
-            return reject(error)
-        }
-    })
-}
+const FrontMatter = require('front-matter')
+const Remark = require('remark')
+const RemarkHTML = require('remark-html')
+const RemarkReact = require('remark-react')
+const React = require('react')
+const Report = require('vfile-reporter')
 
 /**
  * Parse markdown and return the body and imports
  * 
  * @param   {string} markdown - Markdown string to be parsed
+ * @param   {object} options  - Options passed to the loader
  * @returns {object}          - HTML and imports
  */
-module.exports = function(markdown) {
-    return parseMarkdown(
-        FrontMatter(markdown)
-    )
+module.exports = function(markdown, options = {}) {
+    let { plugins = [] } = options,
+        parsed = FrontMatter(markdown)
+
+    return new Promise((resolve, reject) => {
+        plugins
+            .reduce((remark, item) => {
+                return remark.use(item)
+            }, Remark())
+            .use(options.output === 'html' ? RemarkHTML : RemarkReact)
+            .process(parsed.body, (err, file) => {
+                let result = {
+                    content: file.contents,
+                    attributes: parsed.attributes
+                }
+
+                if (err) {
+                    reject( Report(err || file) )
+
+                } else resolve( result )
+            })
+    })
 }
