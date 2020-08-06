8b976ef1362ca68bbc9f3c214ea7a1cdbc031df3
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2020-08-06 20:07:50 +0300

    refactor: code (#24)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { getOptions } from 'loader-utils';
 
 import validateOptions from 'schema-utils';
-import FrontMatter from 'front-matter';
-import Remark from 'remark';
+import frontMatter from 'front-matter';
+import remark from 'remark';
 import Report from 'vfile-reporter';
 
 import schema from './options.json';
@@ -15,56 +15,49 @@ export default function loader(content) {
     baseDataPath: 'options',
   });
 
-  const callback = this.async();
+  const remarkOptions =
+    typeof options.remarkOptions !== 'undefined' ? options.remarkOptions : {};
+  const processor = remark();
+
+  if (typeof remarkOptions.plugins !== 'undefined') {
+    for (const item of remarkOptions.plugins) {
+      if (Array.isArray(item)) {
+        const [plugin, pluginOptions] = item;
+
+        processor.use(plugin, pluginOptions);
+      } else {
+        processor.use(item);
+      }
+    }
+  }
+
+  if (typeof remarkOptions.settings !== 'undefined') {
+    processor.use({ settings: remarkOptions.settings });
+  }
+
+  if (typeof remarkOptions.data !== 'undefined') {
+    processor.data(remarkOptions.data);
+  }
 
   const removeFrontMatter =
     typeof options.removeFrontMatter !== 'undefined'
       ? options.removeFrontMatter
       : true;
-
-  const parsed = removeFrontMatter ? FrontMatter(content) : { body: content };
-
-  const remark = Remark();
-
-  const { remarkOptions = {} } = options;
-
-  const { plugins = [] } = remarkOptions;
-
-  for (const item of plugins) {
-    if (Array.isArray(item)) {
-      const [plugin, pluginOptions] = item;
-
-      remark.use(plugin, pluginOptions);
-    } else {
-      remark.use(item);
-    }
-  }
-
-  const { settings, data } = remarkOptions;
-
-  if (typeof settings !== 'undefined') {
-    remark.use({ settings });
-  }
-
-  if (typeof data !== 'undefined') {
-    remark.data(data);
-  }
+  const callback = this.async();
 
   try {
-    remark.process(parsed.body, (err, file) => {
-      if (err) {
-        callback(Report(err));
+    processor.process(
+      removeFrontMatter ? frontMatter(content).body : content,
+      (error, file) => {
+        if (error) {
+          callback(Report(error));
 
-        return;
+          return;
+        }
+
+        callback(null, file.contents);
       }
-
-      const result = {
-        content: file.contents,
-        attributes: parsed.attributes,
-      };
-
-      callback(null, result.content);
-    });
+    );
   } catch (error) {
     callback(error);
   }
