c1dcc97d3bcd327121ac9e2794b49887f90acd87
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2021-02-01 13:47:21 +0300

    refactor: test (#34)

--- test/helpers/getExecutedCode.js
+++ test/helpers/getExecutedCode.js
@@ -1,17 +1,4 @@
 import { execute, readAsset } from "./index";
 
-export default (asset, compiler, stats) => {
-  let executed = execute(readAsset(asset, compiler, stats));
-
-  if (Array.isArray(executed)) {
-    executed = executed.map((module) => {
-      // Todo remove after drop webpack@4
-      // eslint-disable-next-line no-param-reassign
-      module[0] = module[0].replace(/\?.*!/g, "?[ident]!");
-
-      return module;
-    });
-  }
-
-  return executed;
-};
+export default (asset, compiler, stats) =>
+  execute(readAsset(asset, compiler, stats));
