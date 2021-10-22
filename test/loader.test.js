ea322114d9b082899975435aadc3ba7383b512cf
evilebottnawi <sheo13666q@gmail.com>
2021-10-22 15:59:28 +0300

    test: debug

--- test/loader.test.js
+++ test/loader.test.js
@@ -199,7 +199,7 @@ describe("loader", () => {
     expect(getWarnings(stats)).toMatchSnapshot("warnings");
   });
 
-  it("should throw error #2", async () => {
+  it.only("should throw error #2", async () => {
     const errorGenerationPlugin = () => () => {
       throw new Error("Error");
     };
